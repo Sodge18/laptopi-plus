496aba993e33cb03819cb4c5d4818756187a3624
Sergej Kaldesić <120308990+Sodge18@users.noreply.github.com>
2025-12-06 15:03:48 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -29,6 +29,47 @@ export default {
       });
     }
 
+    // FORMA
+    if (url.pathname === "/send" && request.method === "POST") {
+      const formData = await request.formData();
+  
+      const ime = formData.get("ime");
+      const prezime = formData.get("prezime");
+      const email = formData.get("email");
+      const broj = formData.get("kontakt-broj");
+      const laptop = formData.get("laptop");
+      const poruka = formData.get("poruka");
+      
+      const message = `
+      Nova poruka sa sajta:
+      
+      Ime: ${ime}
+      Prezime: ${prezime}
+      Email: ${email}
+      Kontakt broj: ${broj}
+      Laptop: ${laptop}
+      Poruka: ${poruka}
+          `;
+      
+          const mail = await fetch("https://api.mailchannels.net/tx/v1/send", {
+              method: "POST",
+              headers: { "content-type": "application/json" },
+              body: JSON.stringify({
+                  personalizations: [{
+                      to: [{ email: "TVOJ-MEJL@GMAIL.COM" }]
+                  }],
+                  from: {
+                      email: "no-reply@laptopiplus.rs",
+                      name: "Laptopi Plus"
+                  },
+                  subject: "Nova poruka sa sajta",
+                  content: [{ type: "text/plain", value: message }]
+              })
+          });
+      
+          return new Response("OK", { status: 200 });
+      }
+
     return new Response("Method not allowed", { status: 405 });
   }
 };
